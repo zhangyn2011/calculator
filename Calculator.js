c7b23cd1f1109c50edb760d871cfe18e6a7261d5
zhangyn2011 <zhangyn2011@163.com>
2014-11-30 16:16:29 +0800

    show input

--- Calculator.js
+++ Calculator.js
@@ -1,12 +1,30 @@
 $(document).ready(function(){
+/*	
 	$('.numberkey').click(function(){
 		var inputNum=$('#result').val($(this).val());
 		$('#result').add (inputNum);
 	});
+*/
+	$('.numberkey').click(function(){
+		var result =$('#result').val()+$(this).val();
+		console.log($(this).val());
+		console.log($('#result').val());
+		console.log(result);
+		$('#result').val(result);
+	});
+	
+	
+	
+	
 });
 
 var result=$('#result').add (inputNum);
+var clickbutton=function (){
+	
 
-;;;;;
+
+	
+}
+
 
 
